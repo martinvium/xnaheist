ab765f5ca61c57617fe3b3722b833dcbb054e2c2
Martin Vium <martin.vium@gmail.com>
2012-01-27 23:53:11 +0100

    Add missing levels

---
+++ xnaheist/xnaheist/xnaheist/Levels/Level.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace xnaheist.Levels
+{
+    interface Level
+    {
+        void Initialize(GameObjectFactory gameObjectFactory);
+    }
+}
---
+++ xnaheist/xnaheist/xnaheist/Levels/LevelOne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xnaheist.Levels
+{
+    class LevelOne : Level
+    {
+        private GameObject _bg;
+
+        public void Initialize(GameObjectFactory gameObjectFactory)
+        {
+            _bg = gameObjectFactory.getBackground("background");
+        }
+    }
+}
